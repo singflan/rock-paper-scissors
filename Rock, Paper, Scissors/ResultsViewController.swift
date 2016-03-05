65692e7809333646a58f22d9b41515792b04de3c
Dustin <singflan@gmail.com>
2016-03-04 22:22:54 -0300

    Making sure all files have been pushed to Github

---
+++ Rock, Paper, Scissors/ResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsViewController.swift
+//  Rock, Paper, Scissors
+//
+//  Created by Dustin Flanary on 2/24/16.
+//  Copyright © 2016 Dustin Flanary. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var resultsImageView: UIImageView!
+    
+    @IBAction func playAgain(sender: AnyObject) {
+        
+    }
+    
+}
--- Rock, Paper, Scissors/ViewController.swift
+++ Rock, Paper, Scissors/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+    // I got this code from the discussion board
+    func generateOpponentHand()->String {
+        var handArray = ["Rock", "Paper","Scissors"]
+            
+        let randomSelection = Int(arc4random_uniform(UInt32(handArray.count)))
+        
+        return handArray[randomSelection]
     }
 
 
